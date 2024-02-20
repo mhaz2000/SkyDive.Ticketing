f764649a521df1931147da6485bf03ac5018ddc1
mhaz2000 <m_78habib@yahoo.com>
2024-02-20 09:01:58 +0330

    Capacity is removed from flight loads

---
+++ FreeFallTicketing.Infrastructure/Migrations/20240220045623_remove-capacity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkyDiveTicketing.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removecapacity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "FlightLoads");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "FlightLoads",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
